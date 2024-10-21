69cc1fa2c328c32870203d28ba1504f5379a4e70
Frederik P. <frepbh@gmail.com>
2024-10-21 13:45:37 +0200

    changed to multi root workspace

--- 1 - name sorting/main.py
+++ 1 - name sorting/main.py
@@ -23,12 +23,13 @@ names = [
     "Archer", "Easton", "Emery", "Maddox", "Faith", "Roman", "Riley"
 ]
 
-# Remove duplicates in names
-names = remove_duplicates(names)
+if __name__ == "__main__":
+    # Remove duplicates in names
+    names = remove_duplicates(names)
 
-print(f"Length sorting: {sort_strings_by_length(names)}\n")
-print(f"Alphabetical sorting: {sort_strings_alphabetically(names)}\n")
-print(f"Occurance of characters in 'names': {get_occurances_of_characters(names, True)}\n")
-plot_char_occurances_in_list(names, "names")
-plot_frequency_word_cloud(get_occurances_of_characters(names, True))
-plot_string_length_analysis(names, "names")
+    print(f"Length sorting: {sort_strings_by_length(names)}\n")
+    print(f"Alphabetical sorting: {sort_strings_alphabetically(names)}\n")
+    print(f"Occurance of characters in 'names': {get_occurances_of_characters(names, True)}\n")
+    plot_char_occurances_in_list(names, "names")
+    plot_frequency_word_cloud(get_occurances_of_characters(names, True))
+    plot_string_length_analysis(names, "names")
